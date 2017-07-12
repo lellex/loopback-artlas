261faf8c98877baae829ef6c6cad17ac7df52795
Alexandra Janin <contact.ajanin@gmail.com>
2017-07-12 17:29:44 +0200

    Add city and try to set country

--- common/models/Exhibition.js
+++ common/models/Exhibition.js
@@ -1,25 +1,47 @@
 'use strict';
 
 module.exports = function(Exhibition) {
-  Exhibition.getByCity = function(city, cb) {
-    Exhibition.app.models.City.findOne({
-      where: {name: city},
-    }, function(errors, cityObj) {
-      Exhibition.find({
-        where: {idCity: cityObj.id},
-        include: [{
-          relation: 'exhibitionIdCityFkeyrel',
-        }],
-      }, function(errors, exhibitions) {
-        cb(null, exhibitions);
-      });
+  function findExhibition(params, cb) {
+    Exhibition.find({
+      where: {and: params},
+      include: ['exhibitionIdCityFkeyrel'],
+    }, function(errors, exhibitions) {
+      cb(null, exhibitions);
     });
+  }
+
+  Exhibition.search = function(title, city, contry, cb) {
+    var params = [];
+
+    if (title) {
+      params.push({title: {regexp: new RegExp(title + '+', 'i')}});
+    }
+
+    if (contry) {
+      params.push({formerCountry: {regexp: new RegExp(contry + '+', 'i')}});
+    }
+
+    if (city) {
+      Exhibition.app.models.City.findOne({
+        where: {name: city},
+        include: ['cityIdStateFkeyrel'],
+      }, function(errors, cityObj) {
+        params.push({idCity: cityObj.id});
+        findExhibition(params, cb);
+      });
+    } else {
+      findExhibition(params, cb);
+    }
   };
 
   Exhibition.remoteMethod(
-    'getByCity', {
-      http: {path: '/getByCity', verb: 'get'},
-      accepts: {arg: 'city', type: 'string', http: {source: 'query'}},
+    'search', {
+      http: {path: '/search', verb: 'get'},
+      accepts: [
+        {arg: 'title', type: 'string', http: {source: 'query'}},
+        {arg: 'city', type: 'string', http: {source: 'query'}},
+        {arg: 'country', type: 'string', http: {source: 'query'}},
+      ],
       returns: {arg: 'exhibition', type: 'object'},
     }
   );
